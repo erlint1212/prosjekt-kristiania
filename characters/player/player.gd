36ada45adac1ae4bef793f745b4a28dea46b8cbd
Steven <steventphung6@gmail.com>
2026-01-31 15:40:40 +0100

    added shotgun attack pattern, added background and potential assets to use

--- characters/player/player.gd
+++ characters/player/player.gd
@@ -26,7 +26,7 @@ func _physics_process(delta: float) -> void:
 
 	# 2. Handle Jump
 	# "ui_accept" is mapped to Spacebar/Enter by default
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = jump_velocity
 
 	# 3. Handle Left/Right Movement
